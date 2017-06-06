70ee5ff5508ca9a9d6ecd48de826c2d4d20b7426
Elizabeth Flournoy <bflournoy@gmail.com>
2017-06-06 09:55:37 -0600

    incomplete

--- caesarShift.js
+++ caesarShift.js
@@ -1,4 +1,18 @@
+function decode(string){
+  let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  let phrase = string.toLowerCase().split('');
+  for(let i = 0; i<phrase.length; i++){
+    if(letters.includes(phrase[i])){
+      return letters[letters.indexOf(phrase[i])+3];
+    }else{
+      return phrase[i];
+    }
+  }
+}
 
+function encode(string){
+  let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  console.log(string.toLowerCase().split(''))
+}
 
-
-module.exports = {}
+module.exports = {decode, encode}
